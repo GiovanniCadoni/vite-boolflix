0fc9f2909853f02f595e67463e5b463551ae81d3
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-30 20:13:07 +0100

    add: film's poster and valutation

--- src/components/AppCardProduct.vue
+++ src/components/AppCardProduct.vue
@@ -18,17 +18,25 @@ export default {
 </script>
     
 <template>
-    
+    <img :src="`https://image.tmdb.org/t/p/w342/${product.poster_path}`" alt="">
     <h3>{{ product.title ? product.title : product.name }}</h3>
     <h4>{{ product.original_title ? product.original_title : product.original_name }}</h4>
     <img class="ms_img-lang" v-if="this.store.flag.includes(product.original_language)" :src="getImagePath(product.original_language)" alt="">
     <p v-else>{{ product.original_language }}</p>
-    <p>{{ product.vote_average }}</p>
+    <ul>
+        <li>
+            <i v-for="(starSolid) in Math.ceil(product.vote_average / 2)" class="fa-solid fa-star"></i>
+            <i v-for="(starVoid) in 5 - Math.ceil(product.vote_average / 2)" class="fa-regular fa-star"></i>
+        </li>
+    </ul>
 
 </template>
 
 <style scoped lang="scss">
 .ms_img-lang{
     width: 120px;
+}
+li{
+    list-style: none;
 }
 </style>
